bfcbf5187fb0271bb0679d4aed8837115f08441b
ArturoAmaya <a1amaya@ucsd.edu>
2022-08-15 18:53:22 -0500

    institution

--- Diff.jl
+++ Diff.jl
@@ -6,7 +6,9 @@ function curricular_diff(curriculum1::Curriculum, curriculum2::Curriculum)
     # compare names
     curriculum1.name == curriculum2.name ? println("Curriculum 1 and Curriculum 2 have the same name: $(curriculum1.name)") : println("Curriculum 1 is named $(curriculum1.name) and Curriculum 2 is named $(curriculum2.name)")
     # compare institution
+    curriculum1.institution == curriculum2.institution ? println("Curriculum 1 and Curriculum 2 have the same institution: $(curriculum1.institution)") : println("Curriculum 1 has institution $(curriculum1.institution) and Curriculum 2 has institution $(curriculum2.institution)")
     # compare degree_type
+
     # compare system_type
     # compare CIP
     # compare num_courses
