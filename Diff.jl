996101bd4ab2423f4164bf09143a24ff49ca12ba
ArturoAmaya <a1amaya@ucsd.edu>
2022-08-15 19:36:49 -0500

    metrics_same flag

--- Diff.jl
+++ Diff.jl
@@ -27,49 +27,58 @@ function curricular_diff(curriculum1::Curriculum, curriculum2::Curriculum)
         basic_metrics(curriculum2)
     catch
     end
+    metrics_same = true
     # complexity and max complexity
     if (curriculum1.metrics["complexity"][1] == curriculum2.metrics["complexity"][1])
         println("Curriculum 1 and Curriculum 2 have the same total complexity: $(curriculum1.metrics["complexity"][1])")
     else
         println("Curriculum 1 has a total complexity score of $(curriculum1.metrics["complexity"][1]) and Curriculum2 has a total complexity score $(curriculum2.metrics["complexity"][1])")
+        metrics_same = false
     end
     if (curriculum1.metrics["max. complexity"] == curriculum2.metrics["max. complexity"])
         println("Curriculum 1 and Curriculum 2 have the same max complexity : $(curriculum1.metrics["max. complexity"])")
     else
         println("Curriculum 1 has a max complexity of $(curriculum1.metrics["max. complexity"]) and Curriculum 2 has a max complexity of $(curriculum2.metrics["max. complexity"])")
+        metrics_same = false
     end
     # centrality and max centrality
     if (curriculum1.metrics["centrality"][1] == curriculum2.metrics["centrality"][1])
         println("Curriculum 1 and Curriculum 2 have the same total centrality: $(curriculum1.metrics["centrality"][1])")
     else
         println("Curriculum 1 has a total centrality score of $(curriculum1.metrics["centrality"][1]) and Curriculum2 has a total centrality score $(curriculum2.metrics["centrality"][1])")
+        metrics_same = false
     end
     if (curriculum1.metrics["max. centrality"] == curriculum2.metrics["max. centrality"])
         println("Curriculum 1 and Curriculum 2 have the same max centrality : $(curriculum1.metrics["max. centrality"])")
     else
         println("Curriculum 1 has a max centrality of $(curriculum1.metrics["max. centrality"]) and Curriculum 2 has a max centrality of $(curriculum2.metrics["max. centrality"])")
+        metrics_same = false
     end
     # blocking factor and max blocking factor
     if (curriculum1.metrics["blocking factor"][1] == curriculum2.metrics["blocking factor"][1])
         println("Curriculum 1 and Curriculum 2 have the same total blocking factor: $(curriculum1.metrics["blocking factor"][1])")
     else
         println("Curriculum 1 has a total blocking factor score of $(curriculum1.metrics["blocking factor"][1]) and Curriculum2 has a total blocking factor score $(curriculum2.metrics["blocking factor"][1])")
+        metrics_same = false
     end
     if (curriculum1.metrics["max. blocking factor"] == curriculum2.metrics["max. blocking factor"])
         println("Curriculum 1 and Curriculum 2 have the same max blocking factor : $(curriculum1.metrics["max. blocking factor"])")
     else
         println("Curriculum 1 has a max blocking factor of $(curriculum1.metrics["max. blocking factor"]) and Curriculum 2 has a max blocking factor of $(curriculum2.metrics["max. blocking factor"])")
+        metrics_same = false
     end
     # delay factor and max delay factor
     if (curriculum1.metrics["delay factor"][1] == curriculum2.metrics["delay factor"][1])
         println("Curriculum 1 and Curriculum 2 have the same total delay factor: $(curriculum1.metrics["delay factor"][1])")
     else
         println("Curriculum 1 has a total delay factor score of $(curriculum1.metrics["delay factor"][1]) and Curriculum2 has a total delay factor score $(curriculum2.metrics["delay factor"][1])")
+        metrics_same = false
     end
     if (curriculum1.metrics["max. delay factor"] == curriculum2.metrics["max. delay factor"])
         println("Curriculum 1 and Curriculum 2 have the same max delay factor : $(curriculum1.metrics["max. delay factor"])")
     else
         println("Curriculum 1 has a max delay factor of $(curriculum1.metrics["max. delay factor"]) and Curriculum 2 has a max delay factor of $(curriculum2.metrics["max. delay factor"])")
+        metrics_same = false
     end
 
     # for each course in curriculum 1, try to find a similarly named course in curriculum 2
