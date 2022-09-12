418852a9218babc87dda6856a2398878f81bd57d
ArturoAmaya <a1amaya@ucsd.edu>
2022-09-12 17:50:30 -0500

    more useful error message

--- Diff.jl
+++ Diff.jl
@@ -501,7 +501,7 @@ function curricular_diff(curriculum1::Curriculum, curriculum2::Curriculum, verbo
                 # TODO: handle small bug in runningTally only containing the end results and no intermediate values
                 #println("explained so far: $(runningTally["complexity"]), $(runningTally["centrality"]), $(runningTally["blocking factor"]), $(runningTally["delay factor"])")
             else
-                println("Something weird here, we have more than one match")
+                println("Something weird here, we have more than one match for $course")
             end
         end
         for course in curriculum2.courses
